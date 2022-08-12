ec370f4650de65b49c5c879dda1418498f792e03
Jose San Mateo <info@joseasanmateo.com>
2022-08-12 02:58:28 -0700

    Attached Prompt to button

--- Develop/script.js
+++ Develop/script.js
@@ -10,30 +10,32 @@ console.log(upperAlpha);
 console.log(numAlpha);
 console.log(specialAlpha);
 
-
-
-
-// Make sure to Assign all variables
-// -lowercase
-// - uppercase
-// {Lower and Uppercase part of the same array}
-// - numeric
-// - special characters
-// {Numeric and Special Characters part of same array}
-
-
-// Assignment Code
+// Assignment Code Variable that selects button div
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+// This is a function that...
+// defines variable password which runs a function called generate password
+// defines a variable passwordText that selects the text area
+// Appends to the text area the value of whatever is written  
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;}
 
-  // Add event listener to generate button
+  var promptStart = function showMessage () {
+    alert("Please Select Password Criteria");
+  }
+
+  // Add event listener to generate button & Generate starter prompt
 generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", promptStart);
+
+
+
+
 
 // *****************************
 // All the Code will be down here
