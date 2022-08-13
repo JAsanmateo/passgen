80b67b48c1a9703e499ef8a4398abec52a2470c2
Jose San Mateo <info@joseasanmateo.com>
2022-08-13 14:26:43 -0700

    Added all char prompts

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,11 @@ var trigger = ("Please select the following criteria");
 var lengthTrigger = ("Password length: 8-125");
 var charTrigger = ("Now select additional password options")
 var errorTrigger = ("Error, Please Start over");
+var upperTrigger = ("Uppercase letters in your password?");
+var lowerTrigger = ("Lowercase Letters in your password?");
+var numTrigger = ("Numbers in your password?");
+var specTrigger = ("Special characters in your password?");
+var finTrigger = ("Would you like to see your final password?")
 
 var passRange = [];
 for (var i = 8; i < 126; i++) {
@@ -49,14 +54,56 @@ function generatePassword() {
   } else if (lengthStart > passMax) {
     alert(errorTrigger);
   } else {
-    var password = [];
+    var passLength = [];
     for (var i = 0; i < lengthStart; i++) {
-      password.push(i);
-    }
-    // var charConfirm = prompt(charTrigger); 
-  }
-  alert(password);
-} 
+      passLength.push(i);
+    }}
+    // This ends the Password Length segment. Correct Array Length stored in passLength
+
+// This begins the segment to choose characters
+var charStart = confirm(charTrigger); 
+  if (charStart === true) {
+    var upperConfirm = confirm(upperTrigger);
+  } else {
+    alert(errorTrigger);}  
+  
+    if (upperConfirm === true) {
+      // Replace with code that includes uppercase letters
+      alert(passLength); 
+    } else {
+      var lowerConfirm = confirm(lowerTrigger);}
+    
+    if (lowerConfirm === true) {
+      // Replace with code to include lowercase letters
+      alert(passLength);
+    } else {
+      var numConfirm = confirm(numTrigger);}
+
+      if (numConfirm === true) {
+        // Replace with code to include numbers
+        alert(passLength);
+      } else {
+        var specConfirm = confirm(specTrigger);
+      }
+
+      if (specConfirm === true) {
+        // Replace with code to include special characters
+        alert(passLength);
+      } else {
+        var finConfirm = confirm(finTrigger);
+      }
+
+      if (finConfirm === true) {
+        // Replace to display final password
+        alert(passLength);
+      } else {
+        alert(passLength);
+      }
+
+}
+
+  
+
 
 
 
