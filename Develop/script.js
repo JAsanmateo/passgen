c32d95a77d3be59e592b615c5fa3f86c773ad3ad
Jose San Mateo <info@joseasanmateo.com>
2022-08-12 20:59:25 -0700

    Finally got the prompt boxes to work

--- Develop/script.js
+++ Develop/script.js
@@ -4,34 +4,56 @@ var upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 var numAlpha = "0123456789".split("")
 var specialAlpha = ["!", "#", "$", "%", "&", "*", "-", ";", "=", "^", "~", "$"];
 
+var trigger = ("Please select the following criteria");
+var lengthTrigger = ("Password length: 8-125");
+var charTrigger = ("Now select additional password options")
+var errorTrigger = ("Error, Please Start over");
 
-console.log(alpha);
-console.log(upperAlpha);
-console.log(numAlpha);
-console.log(specialAlpha);
+var passRange = [];
+for (var i = 8; i < 126; i++) {
+  passRange.push(i);
+}
+var passMin = passRange[0];
+var passMax = passRange[117];
+
+
+console.log(passMax);
 
 // Assignment Code Variable that selects button div
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-// This is a function that...
-// defines variable password which runs a function called generate password
-// defines a variable passwordText that selects the text area
-// Appends to the text area the value of whatever is written  
-
+// This function generates the password and displays it in window
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;}
+  passwordText.value = password;
+}
 
-  var promptStart = function showMessage () {
-    alert("Please Select Password Criteria");
-  }
-
-  // Add event listener to generate button & Generate starter prompt
 generateBtn.addEventListener("click", writePassword);
-generateBtn.addEventListener("click", promptStart);
+
+
+function generatePassword() {
+  // This activates the Prompt boxes
+  var start = confirm(trigger); 
+  if (start === true) {
+    var lengthStart = prompt(lengthTrigger);
+  } else if (start !== true) { 
+    var lengthStart = prompt(lengthTrigger);
+  } else {
+    alert(errorTrigger);
+  }
+  // This ends the Starting Prompt
+  // This begins the starting point of password length
+  if (lengthStart < passMin) {
+    alert(errorTrigger);
+  } else if (lengthStart > passMax) {
+    alert(errorTrigger);
+  } else {
+    var charConfirm = prompt(charTrigger); 
+  }
+} 
+
 
 
 
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 1st Commit - Added all challenge files and folder + Comments on what to work on. 
 2nd Commit - Added variables to represent Alphabet and converted strings to Upper and Lower Case.
 3rd/4th Commit - Finally got Working variables for Alphanumerics + Special Chars. Converted Numbers,Letters Special Characters into arrays
+5th commit - Created Iterator for password length, converted it into variables for minimum and maximum. Polished off the prompt boxes so the trigger works and it properly asks for password length. 
